90aa3c86e212fcb36703940a8c0e59b5b7b9034f
Procrastinator <aayanlamba@icloud.com>
2016-08-18 19:57:55 -0400

    3rd commit

---
+++ stock.py
@@ -0,0 +1,19 @@
+import time;
+import serial;
+from urllib import urlopen
+import xml.etree.ElementTree as ET
+
+ser = serial.Serial('COM3',9600);
+while True:
+	u = urlopen('http://localhost:8167/?format=xml');     //From httpspeedfan
+	doc = ET.parse(u);
+	root = doc.getroot();
+	
+	GPUtemp = root[1][12].text;			#Second number is found in configure/Temperatures in speedfan, count to where GPU is, subtract one and replace the second number
+	GPUfanspeed = root[2][6].text;      #Same thing but in configure/fans
+	
+	print (GPUtemp)
+	print (GPUfanspeed)
+	
+	ser.write(GPUtemp+'*'+GPUfanspeed);
+	time.sleep(7.5);
